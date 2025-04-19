9ef0f7590a9b8d4b4284ee4bd813c82d814deceb
Harshwardhan Deshmukh <harshwardhanet7007@gmail.com>
2025-04-19 18:32:23 +0530

    javac added for quick compilation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 local keymap = vim.keymap.set
 
+-- move selection up or down
 keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
@@ -17,10 +18,12 @@ keymap("n", "<leader>O", "O<Esc>j", { desc = "Add new line above (stay in place)
 
 keymap("x", "<leader>p", '"_dP', { desc = "Paste over selection without losing clipboard" })
 
+-- quick replace a word using %s/<word_before>/<word_after>/g
 keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
   desc = "Search & replace word under cursor",
 })
 
+-- Window Resize
 keymap("n", "<S-Left>", ":vertical resize -5<CR>", { desc = "Resize split ← (narrower)" })
 keymap("n", "<S-Right>", ":vertical resize +5<CR>", { desc = "Resize split → (wider)" })
 keymap("n", "<S-Up>", ":resize +2<CR>", { desc = "Resize split ↑ (taller)" })
@@ -28,3 +31,8 @@ keymap("n", "<S-Down>", ":resize -2<CR>", { desc = "Resize split ↓ (shorter)" 
 
 -- add below
 keymap("n", "<leader>ab", "Yp^", { desc = "Duplicate line and jump to first char" })
+
+-- Run Javac for all the programs within the project and output them in out folder
+keymap("n", "<leader>jc", function()
+  vim.cmd("belowright split | terminal javac -d out $(find . -name '*.java')")
+end, { desc = "Compile all Java files to out folder" })
